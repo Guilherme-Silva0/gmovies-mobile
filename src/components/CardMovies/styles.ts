d8f16f904887cbfc0257de0478c83ce79d13c7a8
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-07-10 19:28:17 -0300

    :lipstick: feat: Changing card size

--- src/components/CardMovies/styles.ts
+++ src/components/CardMovies/styles.ts
@@ -2,16 +2,16 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   cardMovie: {
-    height: 145,
-    width: 100,
+    height: 185,
+    width: 125,
     borderRadius: 15,
     marginHorizontal: 5,
     marginBottom: 10,
     backgroundColor: '#424242',
   },
   cardImage: {
-    height: 145,
-    width: 100,
+    height: 185,
+    width: 125,
     borderRadius: 15,
   },
 })
