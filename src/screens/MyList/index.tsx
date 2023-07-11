576b25b97d64bbc2daf6412b3a4596de6871bf4a
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-07-11 20:48:56 -0300

    :sparkles: feat: Creating MyList screen

---
+++ src/screens/MyList/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export function MyList() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>MyList</Text>
+    </View>
+  )
+}
