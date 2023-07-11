0de066155f9a56499485709076154fb3d988e9b0
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-07-11 20:48:40 -0300

    :sparkles: feat: Creating Details screen

---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export function Deatails() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details</Text>
+    </View>
+  )
+}
